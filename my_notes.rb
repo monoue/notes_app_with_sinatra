1b0612815b960d62bfa1c67a75b9b395628e8cc8
monoue <like_a_newborn_baby@icloud.com>
2021-06-29 15:08:04 +0900

    improve: applied RuboCop

--- my_notes.rb
+++ my_notes.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'sinatra'
 require 'sinatra/reloader'
@@ -7,50 +8,10 @@ require 'json'
 require 'securerandom'
 require 'date'
 
-# set :environment, :production
 set :show_exceptions, :after_handler if :environment == :production
 
 error do
-  'エラーが発生しました。 - ' + env['sinatra.error'].message
-end
-
-def get_json_path
-  './notes.json'
-end
-
-def get_target_note_from_notes(notes, note_id)
-  notes.find { |note| note['id'] == note_id }
-end
-
-def get_json_data
-  open(get_json_path) { |io| JSON.load(io) }
-end
-
-def get_target_note(id)
-  json_data = get_json_data
-  get_target_note_from_notes(json_data['notes'], id)
-end
-
-get '/home' do
-  @title = 'ホーム / My Notes'
-  json_data = get_json_data
-  @notes = json_data['notes']
-  erb :home
-end
-
-def make_new_note(params, id)
-  title = params[:title] == '' ? '（無題）' : params[:title]
-  content = params[:content]
-  time = Time.now.to_i
-  {"id"=> id, "title"=> title, "content"=> content, "time"=> time}
-end
-
-def add_note_to_json(params, id = SecureRandom.uuid)
-  json_data = get_json_data
-  new_note = make_new_note(params, id)
-  json_data['notes'] << new_note
-  json_data['notes'].sort_by! { |note| note["time"] }.reverse!
-  open(get_json_path, 'w') { |io| JSON.dump(json_data, io) }
+  "エラーが発生しました。 - #{env['sinatra.error'].message}"
 end
 
 not_found do
@@ -58,9 +19,74 @@ not_found do
   erb :page_not_found
 end
 
+def make_json_path
+  './notes.json'
+end
+
+def make_json_data
+  JSON.parse(File.open(make_json_path).read)
+end
+
+def get_target_note_from_notes(notes, note_id)
+  notes.find { |note| note['id'] == note_id }
+end
+
+def get_target_note(id)
+  json_data = make_json_data
+  get_target_note_from_notes(json_data['notes'], id)
+end
+
+get '/home' do
+  @title = 'ホーム / My Notes'
+  json_data = make_json_data
+  @notes = json_data['notes']
+  erb :home
+end
+
+module Add
+  class << self
+    def add_note_to_json(params, id = SecureRandom.uuid)
+      json_data = make_json_data
+      new_note = make_new_note(params, id)
+      json_data['notes'] << new_note
+      json_data['notes'].sort_by! { |note| note['time'] }.reverse!
+      File.open(make_json_path, 'w') { |io| JSON.dump(json_data, io) }
+    end
+
+    private
+
+    def make_new_note(params, id)
+      title = params[:title] == '' ? '（無題）' : params[:title]
+      content = params[:content]
+      time = Time.now.to_i
+      { 'id' => id, 'title' => title, 'content' => content, 'time' => time }
+    end
+  end
+end
+
+module Delete
+  class << self
+    def delete_note_from_json(id)
+      json_data = make_json_data
+      target_note = get_target_note_from_notes(json_data['notes'], id)
+      json_data['notes'].delete(target_note)
+      File.open(make_json_path, 'w') { |io| JSON.dump(json_data, io) }
+    end
+
+    private
+
+    def get_target_deleted_json_data(id)
+      json_data = make_json_data
+      target_note = get_target_note_from_notes(json_data['notes'], id)
+      json_data['notes'].delete(target_note)
+      json_data
+    end
+  end
+end
+
 post '/new' do
-  add_note_to_json(params)
-  redirect to ('/home')
+  Add.add_note_to_json(params)
+  redirect to('/home')
 end
 
 get '/notes/:id' do |id|
@@ -77,39 +103,21 @@ get '/notes/:id/edit' do |id|
   erb :edit
 end
 
-def get_target_deleted_json_data(id)
-  json_data = get_json_data
-  target_note = get_target_note_from_notes(json_data['notes'], id)
-  json_data['notes'].delete(target_note)
-  json_data
-end
-
-def delete_note_from_json(id)
-  json_data = get_json_data
-  target_note = get_target_note_from_notes(json_data['notes'], id)
-  json_data['notes'].delete(target_note)
-  open(get_json_path, 'w') { |io| JSON.dump(json_data, io) }
-end
-
 patch '/notes/:id/edit' do |id|
-  delete_note_from_json(id)
-  add_note_to_json(params, id)
-  redirect to ('/home')
+  Delete.delete_note_from_json(id)
+  Add.add_note_to_json(params, id)
+  redirect to('/home')
 end
 
-def get_json_data
-  open(get_json_path) { |io| JSON.load(io) }
-end
-
-patch '/notes/:id' do |id|
-  target_deleted_json_data = get_target_deleted_json_data(id)
-  open(get_json_path, 'w') { |io| JSON.dump(target_deleted_json_data, io) }
-  redirect to ('/home')
-end
+# patch '/notes/:id' do |id|
+#   target_deleted_json_data = get_target_deleted_json_data(id)
+#   File.open(make_json_path, 'w') { |io| JSON.dump(target_deleted_json_data, io) }
+#   redirect to('/home')
+# end
 
 delete '/notes/:id' do |id|
-  delete_note_from_json(id)
-  redirect to ('/home')
+  Delete.delete_note_from_json(id)
+  redirect to('/home')
 end
 
 get '/new' do
