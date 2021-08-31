6f0ab318640b3b298c0453a1ca8ea738515ed4e1
monoue <like_a_newborn_baby@icloud.com>
2021-08-31 12:25:35 +0900

    improve: function name, readability, RESTful path

--- my_notes.rb
+++ my_notes.rb
@@ -15,37 +15,6 @@ helpers do
   end
 end
 
-module Connect
-  class << self
-    def make_connection
-      PG.connect(dbname: make_db_name)
-    end
-
-    private
-
-    def make_db_name
-      'my_notes'
-    end
-  end
-end
-
-def make_table_name
-  'notes'
-end
-
-def make_app_name
-  'My Notes'
-end
-
-def make_home_path
-  '/home'
-end
-
-def get_target_note(connection, id)
-  sql = "SELECT * FROM #{make_table_name} WHERE id = $1"
-  connection.exec(sql, [id]).first
-end
-
 error do
   "エラーが発生しました。 - #{env['sinatra.error'].message}"
 end
@@ -55,57 +24,139 @@ not_found do
   erb :page_not_found
 end
 
-connection = Connect.make_connection
-
-get make_home_path do
-  @title = "ホーム / #{make_app_name}"
-  @notes = connection.exec("SELECT * FROM #{make_table_name} ORDER BY timestamp DESC")
-  erb :home
+def app_name
+  'My Notes'
 end
 
-get '/new' do
-  @title = "新規メモの追加 / #{make_app_name}"
+def table_name
+  'notes'
+end
+
+def home_path
+  '/home'
+end
+
+module Connect
+  class << self
+    def make_connection
+      PG.connect(dbname: db_name)
+    end
+
+    private
+
+    def db_name
+      'my_notes'
+    end
+  end
+end
+
+def make_page_title(description)
+  "#{description} / #{app_name}"
+end
+
+def get_target_note(connection, id)
+  sql = "SELECT * FROM #{table_name} WHERE id = $1"
+  connection.exec(sql, [id]).first
+end
+
+connection = Connect.make_connection
+
+module Validate
+  class << self
+    def note_invalid?(note)
+      return true if note.nil?
+
+      [note['id'], note['title'], note['timestamp']].include?(nil)
+    end
+
+    def target_note_invalid?(id)
+      target_note = get_target_note(Connect.make_connection, id)
+      note_invalid?(target_note)
+    end
+
+    def notes_invalid?(notes)
+      notes.any? { |note| note_invalid?(note) }
+    end
+  end
+end
+
+module Note
+  class << self
+    def add_note(connection, params)
+      title = params[:title] == '' ? '（無題）' : params[:title]
+      connection.exec(
+        "INSERT INTO #{table_name} (title, content) VALUES ($1, $2)",
+        [title, params[:content]]
+      )
+    end
+
+    def delete_note(connection, id)
+      connection.exec("DELETE FROM #{table_name} WHERE id = $1", [id])
+    end
+
+    def update_note(connection, params, id)
+      connection.exec(
+        "UPDATE #{table_name} SET title = $1, content = $2 WHERE id = $3",
+        [params[:title], params[:content], id]
+      )
+    end
+  end
+end
+
+get home_path do
+  @title = make_page_title('ホーム')
+  @notes = connection.exec("SELECT * FROM #{table_name} ORDER BY timestamp DESC")
+  if Validate.notes_invalid?(@notes)
+    erb :page_not_found
+  else
+    erb :home
+  end
+end
+
+get '/notes/new' do
+  @title = make_page_title('新規メモの追加')
   erb :new
 end
 
-def add_note(connection, params)
-  title = params[:title] == '' ? '（無題）' : params[:title]
-  connection.exec("INSERT INTO #{make_table_name} (title, content) VALUES ($1, $2)", [title, params[:content]])
-end
-
-post '/new' do
-  add_note(connection, params)
-  redirect to(make_home_path)
+post '/notes' do
+  Note.add_note(connection, params)
+  redirect to(home_path)
 end
 
 get '/notes/:id' do |id|
   @note = get_target_note(connection, id)
-  @title = "メモ: #{@note['title']} / #{make_app_name}"
-  erb :note
-rescue NoMethodError
-  erb :page_not_found
+  if Validate.note_invalid?(@note)
+    erb :page_not_found
+  else
+    @title = make_page_title("メモ: #{@note['title']}")
+    erb :note
+  end
 end
 
 get '/notes/:id/edit' do |id|
   @note = get_target_note(connection, id)
-  @title = "変更: #{@note['title']} / #{make_app_name}"
-  erb :edit
-end
-
-def delete_note(connection, id)
-  connection.exec("DELETE FROM #{make_table_name} WHERE id = $1", [id])
+  if Validate.note_invalid?(@note)
+    erb :page_not_found
+  else
+    @title = make_page_title("変更: #{@note['title']}")
+    erb :edit
+  end
 end
 
 delete '/notes/:id' do |id|
-  delete_note(connection, id)
-  redirect to(make_home_path)
-end
-
-def update_target_note(connection, params, id)
-  connection.exec("UPDATE #{make_table_name} SET title = $1, content = $2 WHERE id = $3", [params[:title], params[:content], id])
+  if Validate.target_note_invalid?(id)
+    erb :page_not_found
+  else
+    Note.delete_note(connection, id)
+    redirect to(home_path)
+  end
 end
 
 patch '/notes/:id/edit' do |id|
-  update_target_note(connection, params, id)
-  redirect to(make_home_path)
+  if Validate.target_note_invalid?(id)
+    erb :page_not_found
+  else
+    Note.update_note(connection, params, id)
+    redirect to(home_path)
+  end
 end
